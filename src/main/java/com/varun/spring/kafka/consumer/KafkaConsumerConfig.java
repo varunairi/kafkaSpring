ef9123051827e7a60f2dfe3c36c6933063b63e64
Airi <varun.airi1@centurylink.com>
2020-05-04 18:00:04 +0530

    Checked in with changes

--- src/main/java/com/varun/spring/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/com/varun/spring/kafka/consumer/KafkaConsumerConfig.java
@@ -1,13 +1,23 @@
 package com.varun.spring.kafka.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerConfig {
 
-    @KafkaListener(groupId = "kafka-bros",topics = "mySpringTopic")
-    public void recieveMessage(String message){
-        System.out.print("Recieved: " + message);
+    @KafkaListener(topics = "mySpringTopic")
+    public void recieveMessage(String message,
+                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+                               @Header(KafkaHeaders.OFFSET) int offset){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Offset: ").append(offset).append(" Partition:").append(partition).append(" with Message:").append(message);
+
+        System.out.println("Recieved: " + sb.toString());
+
+
+
     }
 }
